c58b61ace923895c65c876201b1fefbce1269a6d
yuheee223 <118239298+yuheee223@users.noreply.github.com>
2025-04-15 23:29:08 +0800

    Add files via upload

---
+++ Reversi.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_Reversi.h"
+#include "test.h"
+#include "SwitchButton.h" 
+
+class Reversi : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Reversi(QWidget *parent = nullptr);
+    ~Reversi();
+
+protected:
+    void showEvent(QShowEvent* event) override;
+
+private:
+    Ui::ReversiClass ui;
+};
